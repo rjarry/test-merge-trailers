b226f5c60d630850290cb6b28867f99ede0f1bfa
Robin Jarry <robin@jarry.cc>
2025-10-13 14:41:23 +0200

    use script
    Signed-off-by: Robin Jarry <robin@jarry.cc>

---
+++ apply.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+: ${PULL_REQUEST?PULL_REQUEST}
+: ${LOGIN?LOGIN}
+
+pr() {
+	local attr=$1
+	echo "$PULL_REQUEST" | jq -r ".$attr"
+}
+
+job_url() {
+	local run_id="$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+	local job_id=$(gh api "repos/$run_id/jobs" --jq ".jobs[] | select(.name==\"$GITHUB_JOB\") | .id")
+	echo "https://github.com/$run_id/job/$job_id"
+}
+
+fail() {
+	set +e
+	gh pr comment $(pr url) -b "error: $*\n\n$(job_url)"
+	exit 1
+}
+
+err() {
+	set +e
+	gh pr comment $(pr url) -b "error: command \`$BASH_COMMAND\` failed\n\n$(job_url)"
+	exit 1
+}
+
+user_name() {
+	local login=$1
+	local name=$(gh api users/$login --jq '.name')
+	if [ -z "$name" ] || [ "$name" = null ]; then
+		fail "user $LOGIN does not expose their full name"
+	fi
+	echo "$name"
+}
+
+email_from_gh() {
+	local login=$1
+	gh api users/$login --jq '.email'
+}
+
+email_from_git() {
+	local name=$1
+	git shortlog -se -w0 --group=author --group=committer \
+		--group=trailer:acked-by --group=trailer:reviewed-by \
+		--group=trailer:reported-by --group=trailer:signed-off-by \
+		--group=trailer:tested-by |
+	sed -En "s/^[[:space:]]+[0-9]+[[:space:]]+$name <([^@]+@[^>]+)>$/\\1/p"
+}
+
+user_email() {
+	local login=$1
+	local name=$2
+	local email=$(email_from_gh "$login")
+	if [ -z "$email" ] || [ "$email" = null ]; then
+		email=$(email_from_git "$name")
+		if [ -z "$email" ]; then
+			fail "user $login does not expose their email and is unknown from git history"
+		fi
+	fi
+	echo "$email"
+}
+
+set -xEe -o pipefail
+trap err ERR
+
+name=$(user_name "$LOGIN")
+email=$(user_email "$LOGIN" "$name")
+git config set user.name "$name"
+git config set user.email "$email"
+
+git remote add pr $(pr head.repo.clone_url)
+git fetch pr
+git checkout -t pr/$(pr head.ref)
+
+tmp=$(mktemp)
+trap "rm -f -- $tmp" EXIT
+
+gh api "repos/$GITHUB_REPOSITORY/pulls/$(pr id)/reviews" --paginate \
+	--jq '.[] | select(.state=="APPROVED") | .user.login' | sort -u |
+while read -r login; do
+	name=$(user_name "$login")
+	email=$(user_email "$login" "$name")
+	echo "Reviewed-by: $name <$email>"
+done >> "$tmp"
+
+gh api "repos/$GITHUB_REPOSITORY/issues/$(pr id)/comments" --paginate \
+	--jq '.[].body | select(test("^(Acked-by|Tested-by|Reviewed-by|Reported-by):\\s*"))' >> "$tmp"
+
+trailers=$(sort -u "$tmp")
+mkdir -p .git/hooks
+cp -af devtools/commit-msg .git/hooks/commit-msg
+
+git log --pretty=fuller $(pr base.ref)..$(pr head.ref)
+
+git rebase $(pr base.ref) --exec \
+	'git log -1 --pretty="%B$trailers" | git commit --amend -F - --no-edit'
+
+git log --pretty=fuller $(pr base.ref)..$(pr head.ref)
+
+git checkout $(pr base.ref)
+git merge --ff-only $(pr head.ref)
+git push origin $(pr head.ref)
+
+gh pr comment $(pr url) -b "Pull request applied with git trailers: $(git log -1 --pretty=%H)\n\n$(job_url)"
